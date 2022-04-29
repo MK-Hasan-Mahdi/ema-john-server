a5870bc58042735fba77f74841cd4547d2636e16
mk-hasan <mkhasan6891@gmail.com>
2022-04-30 00:54:00 +0600

    pagination purpose some modified

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,10 +12,43 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygqb1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
+        await client.connect();
+        const productCollection = client.db('emaJohn').collection('product');
 
+        app.get('/product', async (req, res) => {
+            console.log('query', req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+
+            const query = {};
+            const cursor = productCollection.find(query);
+            // const products = await cursor.toArray();
+            let products;
+            if (page || size) {
+                // 0 --> skip: 0 get: 0-10(10): 
+                // 1 --> skip: 1*10 get: 11-20(10):
+                // 2 --> skip: 2*10 get: 21-30 (10):
+                // 3 --> skip: 3*10 get: 21-30 (10):
+                products = await cursor.skip(page * size).limit(size).toArray();
+            }
+            else {
+                products = await cursor.toArray();
+            }
+            res.send(products);
+        })
+
+        // product count
+        app.get('/productCount', async (req, res) => {
+            // const query = {};
+            // const cursor = productCollection.find(query);
+
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count });
+        })
     }
     finally {
 
