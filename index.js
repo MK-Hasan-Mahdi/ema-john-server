d205d646fc24ca19ac5b0465ada4e6bae18387b4
mk-hasan <mkhasan6891@gmail.com>
2022-04-30 03:01:38 +0600

    remain cart total while paginating

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -48,6 +48,17 @@ async function run() {
 
             const count = await productCollection.estimatedDocumentCount();
             res.send({ count });
+        });
+
+        // use post to get products by ids
+        app.post('/productByKeys', async (req, res) => {
+            const keys = req.body;
+            const ids = keys.map(id => ObjectId(id));
+            const query = { _id: { $in: ids } }
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            console.log(keys);
+            res.send(products);
         })
     }
     finally {
